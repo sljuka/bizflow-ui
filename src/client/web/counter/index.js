f1068da3a8188e083724a246f1bd6c6d0a866aed
David Sljukic <dsljukic@blueberryapps.com>
2016-06-04 12:37:53 +0200

    Use stateless function instead of class for counter implementation

--- src/client/web/counter/index.js
+++ src/client/web/counter/index.js
@@ -1,35 +1,35 @@
-import React, { PropTypes as RPT, Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import * as counterActions from '../../common/counter/actions';
+import React, { PropTypes as RPT } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-@connect(
+function Counter(props) {
+  const { actions, counter } = props;
+
+  return (
+    <div>
+      <h2>Counter: {counter}</h2>
+      <button onClick={actions.increment}>inc</button>
+      <button onClick={actions.decrement}>dec</button>
+      <button onClick={actions.magicAction2}>asyncss</button>
+    </div>
+ );
+}
+
+Counter.propTypes = {
+  counter: RPT.number,
+  actions: RPT.object
+};
+
+export default connect(
   (state) => {
     const counter = state.counter.counter;
+
     return { counter };
   },
   (dispatch) => {
     const actions = bindActionCreators(counterActions, dispatch);
+
     return { actions };
   }
-)
-export default class Counter extends Component {
-
-  static propTypes = {
-    counter: RPT.number,
-    actions: RPT.object
-  }
-
-  render() {
-    const { actions } = this.props;
-
-    return (
-      <div>
-        <h2>Counter: {this.props.counter}</h2>
-        <button onClick={actions.increment}>inc</button>
-        <button onClick={actions.decrement}>dec</button>
-        <button onClick={actions.magicAction2}>asyncss</button>
-      </div>
-   );
-  }
-}
+)(Counter);
